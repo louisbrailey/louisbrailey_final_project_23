8969c175a10cf9729069b4ea9c9733e380aca4b3
louisbrailey <louis.brailey@gmail.com>
2023-12-01 09:20:44 -0800

    FIrst push from github desktop

---
+++ Final Project/main.py
@@ -0,0 +1,11 @@
+#this file was created by Louis Brailey
+'''
+game kinda like super mario
+mods that deal damage
+power ups that heal
+moves horizontally
+obstacles
+finish line with a flag
+
+'''
+#i am changing something....
